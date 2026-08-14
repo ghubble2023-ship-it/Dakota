e5c7fcf83bfebad125394fe9abde44de4d8ee453
ghubble2023-ship-it <ghubble2023@gmail.com>
2026-08-14 00:05:22 -0700

    Export engine and new spatial helpers

--- src/gravity_check/__init__.py
+++ src/gravity_check/__init__.py
@@ -1,11 +1,22 @@
 """Gravity Check - Physics-first image analysis package."""
 
-from .spatial_measurement import spatial_report, estimate_relative_scale, check_depth_ordering
-from .shadow_direction import analyze_shadow_consistency
+from .spatial_measurement import (
+    spatial_report,
+    estimate_relative_scale,
+    check_depth_ordering,
+    estimate_vanishing_point,
+    check_perspective_consistency,
+)
+from .shadow_direction import analyze_shadow_consistency, shadow_vector_from_points
+from .engine import run_gravity_check
 
 __all__ = [
     "spatial_report",
     "estimate_relative_scale",
     "check_depth_ordering",
+    "estimate_vanishing_point",
+    "check_perspective_consistency",
     "analyze_shadow_consistency",
+    "shadow_vector_from_points",
+    "run_gravity_check",
 ]
