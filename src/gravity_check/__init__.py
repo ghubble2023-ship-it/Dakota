726a1e517ab38a76433cf747c66f00dcd3a06ca9
ghubble2023-ship-it <ghubble2023@gmail.com>
2026-08-14 00:19:09 -0700

    Export measurement interface

--- src/gravity_check/__init__.py
+++ src/gravity_check/__init__.py
@@ -9,14 +9,34 @@ from .spatial_measurement import (
 )
 from .shadow_direction import analyze_shadow_consistency, shadow_vector_from_points
 from .engine import run_gravity_check
+from .measurement import (
+    ImageMeasurements,
+    ObjectMeasurement,
+    ShadowMeasurement,
+    PerspectiveLine,
+    measure_shadow_from_points,
+    measure_object_from_bbox,
+    feed_to_engine,
+)
 
 __all__ = [
+    # Spatial
     "spatial_report",
     "estimate_relative_scale",
     "check_depth_ordering",
     "estimate_vanishing_point",
     "check_perspective_consistency",
+    # Shadow
     "analyze_shadow_consistency",
     "shadow_vector_from_points",
+    # Engine
     "run_gravity_check",
+    # Measurement front-end
+    "ImageMeasurements",
+    "ObjectMeasurement",
+    "ShadowMeasurement",
+    "PerspectiveLine",
+    "measure_shadow_from_points",
+    "measure_object_from_bbox",
+    "feed_to_engine",
 ]
