51b747254d188ca35d0b47a45bbbd9825e35164e
ghubble2023-ship-it <ghubble2023@gmail.com>
2026-08-13 20:31:33 -0700

    Export core Gravity Check modules from package

--- src/gravity_check/__init__.py
+++ src/gravity_check/__init__.py
@@ -1,1 +1,11 @@
 """Gravity Check - Physics-first image analysis package."""
+
+from .spatial_measurement import spatial_report, estimate_relative_scale, check_depth_ordering
+from .shadow_direction import analyze_shadow_consistency
+
+__all__ = [
+    "spatial_report",
+    "estimate_relative_scale",
+    "check_depth_ordering",
+    "analyze_shadow_consistency",
+]
